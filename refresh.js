75c0518b56271fafbb99a02421d7d0fa57c970bc
talesm <tales.miranda84@gmail.com>
2025-03-04 12:40:00 -0300

    Create refresh.js in substitution of refresh.sh
    - Support to filtering out from update

---
+++ refresh.js
@@ -0,0 +1,31 @@
+const process = require('node:process');
+const { spawnSync } = require("node:child_process");
+const config = require("./scripts/config.json");
+
+/**
+ * Process the main
+ * @param {string[]} args the arguments
+ */
+async function main(args) {
+  process.chdir(__dirname);
+  var { status } = spawnSync("node", ["scripts/", "parse", "scripts/config.json", "-d", "/usr/local/include/SDL3/"], { stdio: 'inherit' });
+  if (status === null) process.exit(1);
+  var { status } = spawnSync("node", ["scripts/", "transform", "scripts/config.json"], { stdio: 'inherit' });
+  if (status === null) process.exit(2);
+
+  const targets = [];
+  if (args?.length) {
+    const filter = new Set(args.map(getRadix));
+    targets.push(...config.sources.map(getRadix).filter(s => !filter.has(s)).map(s => `SDL3pp_${s}.h`));
+    console.log(targets);
+  }
+  var { status } = spawnSync("node", ["scripts/", "update", "scripts/config.json", ...targets], { stdio: 'inherit' });
+  if (status === null) process.exit(3);
+}
+
+/** @param {string} name  */
+function getRadix(name) {
+  return /^(?:SDL(?:3pp)?_)?([^.]+)(?:\.h)?$/.exec(name)?.[1] ?? "";
+}
+
+main(process.argv.slice(2));
--- refresh.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/bash
-node scripts/ parse scripts/config.json -d /usr/local/include/SDL3/
-node scripts/ transform scripts/config.json
-node scripts/ update scripts/config.json
