727143152a4cbd8c1d4698d8e37947b686f2e210
talesm <tales.miranda84@gmail.com>
2025-04-01 10:27:19 -0300

    Add CategoryPlatform

--- docs/html/search/groups_d.js
+++ docs/html/search/groups_d.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['pixel_20formats_20and_20conversion_20routines_0',['Pixel Formats and Conversion Routines',['../group__CategoryPixels.html',1,'']]],
   ['platform_20and_20cpu_20information_1',['Platform and CPU Information',['../group__CategoriesPlatformAndCpuInformation.html',1,'']]],
-  ['pointer_20wrapper_20to_20sdl_3a_3afree_2',['Pointer wrapper to SDL::free()',['../group__CategoryOwnPtr.html',1,'']]],
-  ['points_3',['Application entry points',['../group__CategoryMain.html',1,'']]],
-  ['properties_4',['Object Properties',['../group__CategoryProperties.html',1,'']]]
+  ['platform_20detection_2',['Platform Detection',['../group__CategoryPlatform.html',1,'']]],
+  ['pointer_20wrapper_20to_20sdl_3a_3afree_3',['Pointer wrapper to SDL::free()',['../group__CategoryOwnPtr.html',1,'']]],
+  ['points_4',['Application entry points',['../group__CategoryMain.html',1,'']]],
+  ['properties_5',['Object Properties',['../group__CategoryProperties.html',1,'']]]
 ];
