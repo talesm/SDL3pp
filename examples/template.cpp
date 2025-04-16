97d130356e279376ce38034c912cb248dbde98c0
talesm <tales.miranda84@gmail.com>
2025-04-16 12:32:55 -0300

    Rename MAIN to template

--- examples/template.cpp
+++ examples/template.cpp
@@ -5,14 +5,10 @@ using namespace std::chrono_literals;
 struct Main
 {
   static constexpr SDL::Point windowSz = {400, 400};
-  static constexpr SDL::FRect characterRect{SDL::FPoint(windowSz) / 2 -
-                                              SDL::FPoint{64, 64},
-                                            {128, 128}};
 
   SDL::SDL init;
   SDL::Window window;
   SDL::Renderer renderer;
-  SDL::Texture characterTexture;
 
   Main(int, char**)
     : init(SDL::INIT_VIDEO)
@@ -20,19 +16,14 @@ struct Main
     if (!init) throw std::runtime_error{SDL::GetError()};
     std::tie(window, renderer) = SDL::CreateWindowAndRenderer("Test", windowSz);
     if (!window) throw std::runtime_error{SDL::GetError()};
-    characterTexture = {renderer, "assets/smiley.png"};
-    if (!characterTexture) throw std::runtime_error{SDL::GetError()};
   }
-
-  // SDL::AppResult Init(int argc, char* argv[]) {}
 
   SDL::AppResult Iterate()
   {
-    renderer.SetDrawColor(SDL::FColor{.5f, .5f, .5f, 1.f});
+    renderer.SetDrawColor(SDL::FColor{.75f, .75f, .75f, 1.f});
     renderer.RenderClear();
     renderer.SetDrawColor(SDL::FColor{0.f, 0.725f, 0.f, 1.f});
     renderer.RenderFillRect(SDL::FRect{10, 10, 380, 380});
-    renderer.RenderTexture(characterTexture, {}, characterRect);
 
     renderer.Present();
     return SDL::APP_CONTINUE;
@@ -47,8 +38,6 @@ struct Main
     }
     return SDL::APP_CONTINUE;
   }
-
-  void Quit(SDL::AppResult) { SDL::Log("Bye"); }
 };
 
 #define SDL3PP_MAIN_USE_THIS_CLASS Main
