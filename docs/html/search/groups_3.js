e7e44057b48af7d85defb355945892bfd1531643
Tales Miranda <tales.miranda84@gmail.com>
2025-05-06 09:02:26 -0300

    Wrap Clipboard (#15)
    * Create demo 01_text
    
    * Fix definition of SDL::mem*
    
    Also allow insert mode
    
    * Wrap clipboard
    
    * Add paste function
    
    * Cut/Copy entire line

--- docs/html/search/groups_3.js
+++ docs/html/search/groups_3.js
@@ -5,11 +5,12 @@ var searchData=
   ['callback_20helpers_2',['Async callback helpers',['../group__CategoryCallbackWrapper.html',1,'']]],
   ['categories_3',['Categories',['../group__Categories.html',1,'']]],
   ['category_20events_4',['Category Events',['../group__CategoryEvents.html',1,'']]],
-  ['compiler_20intrinsics_20detection_5',['Compiler Intrinsics Detection',['../group__CategoryIntrinsics.html',1,'']]],
-  ['configuration_20variables_6',['Configuration Variables',['../group__CategoryHints.html',1,'']]],
-  ['control_7',['Process Control',['../group__CategoryProcess.html',1,'']]],
-  ['conversion_20routines_8',['Pixel Formats and Conversion Routines',['../group__CategoryPixels.html',1,'']]],
-  ['cpu_20feature_20detection_9',['CPU Feature Detection',['../group__CategoryCPUInfo.html',1,'']]],
-  ['cpu_20information_10',['Platform and CPU Information',['../group__CategoriesPlatformAndCpuInformation.html',1,'']]],
-  ['creation_20and_20simple_20drawing_11',['Surface Creation and Simple Drawing',['../group__CategorySurface.html',1,'']]]
+  ['clipboard_20handling_5',['Clipboard Handling',['../group__CategoryClipboard.html',1,'']]],
+  ['compiler_20intrinsics_20detection_6',['Compiler Intrinsics Detection',['../group__CategoryIntrinsics.html',1,'']]],
+  ['configuration_20variables_7',['Configuration Variables',['../group__CategoryHints.html',1,'']]],
+  ['control_8',['Process Control',['../group__CategoryProcess.html',1,'']]],
+  ['conversion_20routines_9',['Pixel Formats and Conversion Routines',['../group__CategoryPixels.html',1,'']]],
+  ['cpu_20feature_20detection_10',['CPU Feature Detection',['../group__CategoryCPUInfo.html',1,'']]],
+  ['cpu_20information_11',['Platform and CPU Information',['../group__CategoriesPlatformAndCpuInformation.html',1,'']]],
+  ['creation_20and_20simple_20drawing_12',['Surface Creation and Simple Drawing',['../group__CategorySurface.html',1,'']]]
 ];
