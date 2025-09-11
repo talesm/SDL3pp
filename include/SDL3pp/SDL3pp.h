57eadabba27cdf099f2358bf55939531707d6c17
talesm <tales.miranda84@gmail.com>
2025-09-11 18:08:45 -0300

    Update SDL3pp.h

--- include/SDL3pp/SDL3pp.h
+++ include/SDL3pp/SDL3pp.h
@@ -34,7 +34,7 @@
 // #include "SDL3pp_guid.h"
 // #include "SDL3pp_haptic.h"
 // #include "SDL3pp_hidapi.h"
-// #include "SDL3pp_hints.h"
+#include "SDL3pp_hints.h"
 #include "SDL3pp_init.h"
 // #include "SDL3pp_intrin.h"
 // #include "SDL3pp_iostream.h"
