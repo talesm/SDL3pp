4d9948d01294d65a5af92f6d018ad8340d28ec2e
talesm <tales.miranda84@gmail.com>
2025-09-11 15:33:54 -0300

    Use default diff format

--- scripts/takeSnapshot.sh
+++ scripts/takeSnapshot.sh
@@ -9,7 +9,7 @@ for file in src/generated/*.h; do
     filename=$(basename "$file")
     echo "updating patch $filename"
     clang-format -i $file
-    diff -u "$file" "include/SDL3pp/$filename" > "src/patches/$filename.patch"
+    diff "$file" "include/SDL3pp/$filename" > "src/patches/$filename.patch"
   fi
 done
 cmake --build  build/ -t SDL3pp_refresh_new
