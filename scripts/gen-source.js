25a31ace6441d8a4df30b0ed022c0a5b8daffe34
talesm <tales.miranda84@gmail.com>
2025-11-05 14:50:48 -0300

    Rename config-source2.json to just config-source.json

--- scripts/gen-source.js
+++ scripts/gen-source.js
@@ -1,5 +1,5 @@
 import { writeJSONSync, system } from "./cppfier/dist/utils.js";
-import sourceConfig2 from "./config-source2.json" with {type: "json"};
+import sourceConfig2 from "./config-source.json" with {type: "json"};
 import { parseApi } from "./cppfier/dist/parse-grammar.js";
 
 system.silent = false;
