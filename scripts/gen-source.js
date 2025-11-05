5195bb397d16e9d4eb1e6d0899d51be0d515c9d4
talesm <tales.miranda84@gmail.com>
2025-11-05 15:02:06 -0300

    Remove now unecessary gen-source.js

--- scripts/gen-source.js
+++
@@ -1,8 +0,0 @@
-import { writeJSONSync, system } from "./cppfier/dist/utils.js";
-import sourceConfig2 from "./config-source.json" with {type: "json"};
-import { parseApi } from "./cppfier/dist/parse.js";
-
-system.silent = false;
-
-const newApi = parseApi(sourceConfig2);
-writeJSONSync("scripts/source.json", newApi);
