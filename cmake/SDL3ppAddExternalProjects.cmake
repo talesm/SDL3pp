1931d69a4dc6a9ebd7fbb93cafaa9916e91a3511
talesm <tales.miranda84@gmail.com>
2025-12-16 11:08:15 -0300

    Support for SDL

--- cmake/SDL3ppAddExternalProjects.cmake
+++ cmake/SDL3ppAddExternalProjects.cmake
@@ -8,10 +8,12 @@ set(SDL3PP_USE_SDL3_TAG OFF CACHE STRING "Bundle SDL3 with this named tag/branch
 set(SDL3PP_USE_SDL3IMAGE_TAG OFF CACHE STRING "Bundle SDL3_image with this named tag/branch")
 set(SDL3PP_USE_SDL3TTF_TAG OFF CACHE STRING "Bundle SDL3_ttf with this named tag/branch")
 
+set(SDL3PP_SDL_DEFAULT_VERSION 3.2.28)
+
 if(SDL3PP_USE_SDL3_TAG)
   set(SDL3_TAG ${SDL3PP_USE_SDL3_TAG})
 else ()
-  set(SDL3_TAG release-3.2.28)
+  set(SDL3_TAG "release-${SDL3PP_SDL_DEFAULT_VERSION}")
 endif ()
 if(SDL3PP_USE_SDL3IMAGE_TAG)
   set(SDL3IMAGE_TAG ${SDL3PP_USE_SDL3IMAGE_TAG})
@@ -33,6 +35,13 @@ option(SDL3PP_USE_WINDOWS_PREBUILT "Download prebuilt instead of sources (Window
 
 if (SDL3PP_USE_WINDOWS_PREBUILT)
   message("Configuring using windows pre-built")
+  FetchContent_Declare(SDL3PreBuilt
+    URL "https://github.com/libsdl-org/SDL/releases/download/release-${SDL3PP_SDL_DEFAULT_VERSION}/SDL3-devel-${SDL3PP_SDL_DEFAULT_VERSION}-VC.zip"
+    UPDATE_DISCONNECTED ${SDL3PP_BUNDLE_DISCONNECTED}
+  )
+  FetchContent_MakeAvailable(SDL3PreBuilt)
+  include("${PROJECT_BINARY_DIR}/_deps/sdl3prebuilt-src/cmake/SDL3Config.cmake")
+
 else()
   set(SDL_INSTALL ON) # passed to external/SDL
   FetchContent_Declare(SDL3External
