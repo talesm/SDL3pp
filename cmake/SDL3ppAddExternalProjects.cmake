d03f2b7d5504224ff4db2ac3da39f04d08061d1a
talesm <tales.miranda84@gmail.com>
2026-01-01 09:49:17 -0300

    Update bundled SDL3 to 3.2.30

--- cmake/SDL3ppAddExternalProjects.cmake
+++ cmake/SDL3ppAddExternalProjects.cmake
@@ -8,7 +8,7 @@ set(SDL3PP_USE_SDL3_TAG OFF CACHE STRING "Bundle SDL3 with this named tag/branch
 if(SDL3PP_USE_SDL3_TAG)
   set(SDL3_TAG ${SDL3PP_USE_SDL3_TAG})
 else ()
-  set(SDL3_TAG release-3.2.28)
+  set(SDL3_TAG release-3.2.30)
 endif ()
 
 set(SDL_INSTALL ON) # passed to external/SDL
