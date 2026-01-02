6ca40e94b8bae8a9b1a04be67eeeba8d0864363e
talesm <tales.miranda84@gmail.com>
2026-01-02 12:41:38 -0300

    Fix MouseMotionTransformCallback signature

--- include/SDL3pp/SDL3pp_mouse.h
+++ include/SDL3pp/SDL3pp_mouse.h
@@ -536,7 +536,7 @@ constexpr MouseButtonFlags ButtonMask(MouseButton button)
  */
 using MouseMotionTransformCallback = void(SDLCALL*)(void* userdata,
                                                     Uint64 timestamp,
-                                                    SDL_Window* window,
+                                                    WindowRaw window,
                                                     MouseID mouseID,
                                                     float* x,
                                                     float* y);
@@ -573,7 +573,7 @@ using MouseMotionTransformCallback = void(SDLCALL*)(void* userdata,
  * @sa MouseMotionTransformCallback
  */
 using MouseMotionTransformCB = MakeFrontCallback<void(Uint64 timestamp,
-                                                      SDL_Window* window,
+                                                      WindowRaw window,
                                                       MouseID mouseID,
                                                       float* x,
                                                       float* y)>;
