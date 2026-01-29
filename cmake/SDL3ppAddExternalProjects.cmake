3e0262e3fc84b09167c73faffb4c348af58adcd8
talesm <tales.miranda84@gmail.com>
2026-01-29 15:30:00 -0300

    Bundled downloads from url instead of cloning repo

--- cmake/SDL3ppAddExternalProjects.cmake
+++ cmake/SDL3ppAddExternalProjects.cmake
@@ -4,11 +4,11 @@ set(SDL3PP_BUNDLE_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external)
 option(SDL3PP_BUNDLE_SHALLOW "Use shallow download" OFF)
 option(SDL3PP_BUNDLE_DISCONNECTED "Use disconnected download" OFF)
 
-set(SDL3PP_USE_SDL3_TAG OFF CACHE STRING "Bundle SDL3 with this named tag/branch")
-if(SDL3PP_USE_SDL3_TAG)
-  set(SDL3_TAG ${SDL3PP_USE_SDL3_TAG})
+set(SDL3PP_USE_SDL3_URL OFF CACHE STRING "Bundle SDL3 with this named tag/branch")
+if(SDL3PP_USE_SDL3_URL)
+  set(SDL3_URL ${SDL3PP_USE_SDL3_URL})
 else ()
-  set(SDL3_TAG release-3.4.0)
+  set(SDL3_URL https://github.com/libsdl-org/SDL/releases/download/release-3.4.0/SDL3-3.4.0.tar.gz)
 endif ()
 
 set(SDL_INSTALL ON) # passed to SDL3External
@@ -17,49 +17,39 @@ set(SDL_TEST_LIBRARY OFF) # passed to SDL3External
 set(SDL_X11_XTEST OFF)  # passed to SDL3External
 
 FetchContent_Declare(SDL3External
-  GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-  GIT_TAG ${SDL3_TAG}
-  GIT_SUBMODULES_RECURSE ON
-  GIT_SHALLOW ${SDL3PP_BUNDLE_SHALLOW}
-  GIT_PROGRESS ON
+  URL ${SDL3_URL}
   OVERRIDE_FIND_PACKAGE
 )
 FetchContent_MakeAvailable(SDL3External)
 
 if (SDL3PP_ENABLE_IMAGE)
   set(SDLIMAGE_INSTALL ON) # passed to external/SDL
-  set(SDL3PP_USE_SDL3IMAGE_TAG OFF CACHE STRING "Bundle SDL3_image with this named tag/branch")
+  set(SDL3PP_USE_SDL3IMAGE_URL OFF CACHE STRING "Bundle SDL3_image with this named tag/branch")
   set(SDL3PP_DEPENDENCIES ${SDL3PP_DEPENDENCIES} SDL3_image::SDL3_image)
-  if(SDL3PP_USE_SDL3IMAGE_TAG)
-    set(SDL3IMAGE_TAG ${SDL3PP_USE_SDL3IMAGE_TAG})
+  if(SDL3PP_USE_SDL3IMAGE_URL)
+    set(SDL3IMAGE_URL ${SDL3PP_USE_SDL3IMAGE_URL})
   else ()
-    set(SDL3IMAGE_TAG release-3.4.0)
+    set(SDL3IMAGE_URL https://github.com/libsdl-org/SDL_image/releases/download/release-3.4.0/SDL3_image-3.4.0.tar.gz)
   endif ()
   FetchContent_Declare(SDL3ImageExternal
-    GIT_REPOSITORY https://github.com/libsdl-org/SDL_image.git
-    GIT_TAG ${SDL3IMAGE_TAG}
-    GIT_SUBMODULES_RECURSE ON
-    GIT_SHALLOW ${SDL3PP_BUNDLE_SHALLOW}
-    GIT_PROGRESS ON
+    URL ${SDL3IMAGE_URL}
+    OVERRIDE_FIND_PACKAGE
   )
   FetchContent_MakeAvailable(SDL3ImageExternal)
 endif (SDL3PP_ENABLE_IMAGE)
 
 if (SDL3PP_ENABLE_TTF)
   set(SDLTTF_INSTALL ON) # passed to external/SDL
-  set(SDL3PP_USE_SDL3TTF_TAG OFF CACHE STRING "Bundle SDL3_ttf with this named tag/branch")
+  set(SDL3PP_USE_SDL3TTF_URL OFF CACHE STRING "Bundle SDL3_ttf with this named tag/branch")
   set(SDL3PP_DEPENDENCIES ${SDL3PP_DEPENDENCIES} SDL3_ttf::SDL3_ttf)
-  if(SDL3PP_USE_SDL3TTF_TAG)
-    set(SDL3TTF_TAG ${SDL3PP_USE_SDL3TTF_TAG})
+  if(SDL3PP_USE_SDL3TTF_URL)
+    set(SDL3TTF_URL ${SDL3PP_USE_SDL3TTF_URL})
   else ()
-    set(SDL3TTF_TAG release-3.2.2)
+    set(SDL3TTF_URL https://github.com/libsdl-org/SDL_ttf/releases/download/release-3.2.2/SDL3_ttf-3.2.2.tar.gz)
   endif ()
   FetchContent_Declare(SDL3TTFExternal
-    GIT_REPOSITORY https://github.com/libsdl-org/SDL_ttf.git
-    GIT_TAG ${SDL3TTF_TAG}
-    GIT_SUBMODULES_RECURSE ON
-    GIT_SHALLOW ${SDL3PP_BUNDLE_SHALLOW}
-    GIT_PROGRESS ON
+    URL ${SDL3TTF_URL}
+    OVERRIDE_FIND_PACKAGE
   )
   FetchContent_MakeAvailable(SDL3TTFExternal)
 endif (SDL3PP_ENABLE_TTF)
