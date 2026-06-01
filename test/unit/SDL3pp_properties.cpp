90f8e6d00cba37aa85267de33a008b47b12a7e8c
talesm <tales.miranda84@gmail.com>
2026-06-01 10:11:17 -0300

    Create PropertyProxy

--- test/unit/SDL3pp_properties.cpp
+++ test/unit/SDL3pp_properties.cpp
@@ -43,4 +43,77 @@ TEST_CASE("Property .Set()")
   CHECK(props.GetPropertyType("HeyPointer") == PROPERTY_TYPE_POINTER);
 }
 
+class PropertyProxy
+{
+  PropertiesRef m_props;
+  StringParam m_name;
+
+public:
+  constexpr PropertyProxy(PropertiesRef props, StringParam name)
+    : m_props(props)
+    , m_name(std::move(name))
+  {
+  }
+
+  bool IsValid() { return m_props.HasProperty(m_name.c_str()); }
+
+  PropertyType GetType() { return m_props.GetPropertyType(m_name.c_str()); }
+
+  const char* GetName() const { return m_name; }
+
+  operator void*()
+  {
+    return m_props.GetPointerProperty(m_name.c_str(), nullptr);
+  }
+  operator const char*()
+  {
+    return m_props.GetStringProperty(m_name.c_str(), "");
+  }
+  template<std::integral T>
+  operator T()
+  {
+    return T(m_props.GetNumberProperty(m_name.c_str(), 0));
+  }
+  operator float() { return m_props.GetFloatProperty(m_name.c_str(), 0); }
+  operator bool() { return m_props.GetBooleanProperty(m_name.c_str(), false); }
+};
+
+TEST_CASE("PropertyProxy")
+{
+  Properties props;
+  PropertyProxy pp{props, "key"};
+  CHECK(pp.IsValid() == false);
+
+  props.Set("key", "value");
+  CHECK(pp.IsValid() == true);
+  CHECK(bool(pp) == true);
+  CHECK(std::string(pp) == "value");
+  CHECK(Sint64(pp) == 0);
+  CHECK(int(pp) == 0);
+  CHECK(float(pp) == 0);
+  CHECK(double(pp) == 0);
+  CHECK((void*)(pp) == nullptr);
+
+  props.Set("key", true);
+  CHECK(bool(pp) == true);
+  CHECK(std::string(pp) == "true");
+  CHECK(Sint64(pp) == 1);
+  CHECK(float(pp) == 1);
+  CHECK((void*)(pp) == nullptr);
+
+  props.Set("key", 10);
+  CHECK(bool(pp) == true);
+  CHECK(std::string(pp) == "10");
+  CHECK(Sint64(pp) == 10);
+  CHECK(float(pp) == 10);
+  CHECK((void*)(pp) == nullptr);
+
+  props.Set("key", 10.5f);
+  CHECK(bool(pp) == true);
+  CHECK(std::string(pp) == "10.500000");
+  CHECK(Sint64(pp) == 11);
+  CHECK(float(pp) == 10.5f);
+  CHECK((void*)(pp) == nullptr);
+}
+
 } // namespace SDL
